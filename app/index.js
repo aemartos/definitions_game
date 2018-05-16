1377f4478efcdfe4e88d37cdb398db68c596f9aa
ebarra <ebarra@dit.upm.es>
2018-05-16 18:45:44 +0200

    first version of boilerplate

---
+++ app/index.js
@@ -0,0 +1,11 @@
+/**
+ * Application entry point
+ */
+
+// Load application styles
+import 'styles/index.scss';
+
+// ================================
+// START YOUR APP HERE
+// ================================
+$("#replaced").html("Hello World23f");
