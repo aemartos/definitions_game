f1ab691174e680948f43f0fd6fcf74b4ae7d71be
ebarra <ebarra@dit.upm.es>
2018-05-16 18:49:03 +0200

    first version of boilerplate

--- app/index.js
+++ app/index.js
@@ -4,8 +4,16 @@
 
 // Load application styles
 import 'styles/index.scss';
+import {city, country, Greeter} from './world';
 
 // ================================
 // START YOUR APP HERE
 // ================================
-$("#replaced").html("Hello World23f");
+
+//Example of simple jquery code working
+$("#replaced").html("Hello World");
+
+//example of creation of new object from other file
+var g = new Greeter("Hola desde");
+
+document.getElementById('output').innerHTML = `${g.greet()} ${city}, ${country()}`;
