02777a6165a9cf85cb79cf294f05c8cd12fba7e9
aemartos <aemartos@gmail.com>
2018-05-25 13:09:15 +0200

    Config, definitions and locales set

--- app/definitions.js
+++ app/definitions.js
@@ -2,26 +2,224 @@
 export const MASTER = {
   countdown: true,
   time: "60:00",
-  letter_limit: 25,
+  letter_limit: 25, //4, 9, 16 or 25
   wildcards: [{name: "additional tip", use: false}, {name: "two tries", use: false}, {name: "number of letters", use: false}, {name: "next letter", use: false}],
   letters: [
     {
       letter: "a",
       starts_or_contains: "start",
       header: "empieza por la",
-      def: "apodo o pseudónimo utilizado en las redes. nombre usualmente corto y fácil de recordar.",
+      def: "apodo o pseudónimo utilizado en las redes. nombre usualmente corto y fácil de recordar",
       answer: "alias",
-      comodines: [],
-      tip: ""
+      tip: "también suele emplearse el anglicismo nick, para designar el mismo concepto"
     },
     {
       letter: "b",
       starts_or_contains: "start",
       header: "empieza por la",
-      def: "imagen, gráfico o texto con fines publicitarios que habitualmente enlaza con el sitio web del anunciante.",
+      def: "imagen, gráfico o texto con fines publicitarios que habitualmente enlaza con el sitio web del anunciante",
       answer: "banner",
-      comodines: [],
+      tip: "de forma rectangular y con orientación horizontal que se coloca muchas veces en el tercio superior de la página web"
+    },
+    {
+      letter: "c",
+      starts_or_contains: "start",
+      header: "empieza por la",
+      def: "navegador web de código abierto más popular, con interfaz sencilla, muy buena estabilidad, velocidad y seguridad",
+      answer: "chrome",
+      tip: "es el navegador más usado de internet, desarrollado por google"
+    },
+    {
+      letter: "c",
+      starts_or_contains: "start",
+      header: "empieza por la",
+      def: "datos que se envian desde un servidor web al navegador del cliente y que se guardan localmente en el pc del usuario",
+      answer: "cookie",
+      tip: "lo utilizan las páginas web para consultar la actividad previa del usuario, llevar un control sobre él y conseguir información sobre sus hábitos"
+    },
+    {
+      letter: "d",
+      starts_or_contains: "start",
+      header: "empieza por la",
+      def: "sistema de denominación de hosts en internet el cual está formado por un conjunto de caracteres el cual identifica un sitio de la red accesible por un usuario",
+      answer: "dominio",
+      tip: "nombre único y exclusivo que se le da a un sitio web en internet para que cualquiera pueda visitarlo"
+    },
+    {
+      letter: "e",
+      starts_or_contains: "start",
+      header: "empieza por la",
+      def: "símbolo gráfico que representa diversas expresiones, permitiendo que una persona puede mostrar su estado de ánimo en medios como la mensajería instantánea ;)",
+      answer: "emoticon",
+      tip: "🙂"
+    },
+    {
+      letter: "f",
+      starts_or_contains: "start",
+      header: "empieza por la",
+      def: "compilación de las preguntas más frecuentes que se hacen en una web de cualquier tema",
+      answer: "FAQ",
+      tip: "expresión inglesa: frequently asked questions"
+    },
+    {
+      letter: "f",
+      starts_or_contains: "start",
+      header: "empieza por la",
+      def: "mini grafico (a modo de marca o logo), que aparece a los usuarios en la barra de direcciones del navegador",
+      answer: "favicon",
+      tip: 'mezcla de las palabras en inglés  "favorite" e "icon"'
+    },
+    {
+      letter: "h",
+      starts_or_contains: "start",
+      header: "empieza por la",
+      def: "persona experta en el manejo de computadoras, que se ocupa de la seguridad de los sistemas y de desarrollar técnicas de mejora, utiliza sus conocimientos técnicos para superar un problema, normalmente asociado a la seguridad",
+      answer: "hacker",
+      tip: "no hay que confundir con cracker, que es el que rompe la seguridad de un sistema"
+    },
+    {
+      letter: "i",
+      starts_or_contains: "start",
+      header: "empieza por la",
+      def: "red informática de nivel mundial que utiliza la línea telefónica para transmitir la información",
+      answer: "internet",
+      tip: "red de redes que comunica ordenadores a través de protocolos IP"
+    },
+    {
+      letter: "j",
+      starts_or_contains: "start",
+      header: "empieza por la",
+      def: "formato de imagen más popular. es un formato comprimido, lo que le permite ocupar poco espacio en la memoria o ser enviado con rapidez por internet",
+      answer: "JPG",
+      tip: 'extensión de archivo de las imágenes, nombre de la imagen, seguido de un punto:  "imagen.xxx"'
+    },
+    {
+      letter: "l",
+      starts_or_contains: "start",
+      header: "empieza por la",
+      def: "red social orientada al ambiente comercial y de negocios",
+      answer: "linkedin",
+      tip: "partiendo del perfil de cada usuario, que libremente revela su experiencia laboral y sus destrezas, esta web pone en contacto a millones de empresas y empleados"
+    },
+    {
+      letter: "l",
+      starts_or_contains: "start",
+      header: "empieza por la",
+      def: "clave de acceso que se le asigna a un usuario, lo identifica dentro de internet junto con la dirección electrónica del pc que utiliza",
+      answer: "login",
+      tip: "en español: entrar o ingresar en un sitio web, a través de tu perfil de usuario"
+    },
+    {
+      letter: "m",
+      starts_or_contains: "start",
+      header: "empieza por la",
+      def: "programa malicioso cuyo objetivo es causar daños a ordenadores, sistemas o redes y, por extensión, a sus usuarios",
+      answer: "malware",
+      tip: "abreviatura de malicious software"
+    },
+    {
+      letter: "n",
+      starts_or_contains: "start",
+      header: "empieza por la",
+      def: "aplicación para visualizar todo tipo de información y navegar por internet con funcionalidades plenamente multimedia",
+      answer: "navegador",
+      tip: "como ejemplo de este tipo de aplicaciones existen: internet explorer, firefox, chrome y safari"
+    },
+    {
+      letter: "o",
+      starts_or_contains: "contain",
+      header: "contiene la",
+      def: "programa cuyo código fuente está disponible al público general, para usar y modificar",
+      answer: "open source",
+      tip: "código fuente abierto o software libre, aunque el software libre no es siempre gratuito"
+    },
+    /*{
+      letter: "p",
+      starts_or_contains: "start",
+      header: "empieza por la",
+      def: "su contenido puede ir desde un texto corto a un conjunto de textos, gráficos estáticos o en movimiento, sonido, etc, en lenguaje HTML",
+      answer: "página web",
       tip: ""
+    },*/
+    {
+      letter: "p",
+      starts_or_contains: "start",
+      header: "empieza por la",
+      def: "es una aplicación web para la transferencia segura de fondos, entre cuentas de miembros",
+      answer: "paypal",
+      tip: "empresa que facilita un servicio virtual de pago. a través de este sistema es posible realizar compras online de forma segura"
+    },
+    {
+      letter: "p",
+      starts_or_contains: "start",
+      header: "empieza por la",
+      def: "comunicaciones fraudulentas diseñadas para inducir a los consumidores a divulgar información personal o financiera, incluyendo nombre de usuario, contraseña, información sobre tarjetas de crédito, entre otros",
+      answer: "phishing",
+      tip: "utilizado por los delincuentes para obtener información confidencial,  haciéndose pasar por una comunicación confiable y legítima"
+    },
+    {
+      letter: "q",
+      starts_or_contains: "start",
+      header: "empieza por la",
+      def: "forma como se le denomina al tipo de teclado que más se usa en la actualidad",
+      answer: "QWERTY",
+      tip: "el nombre viene de la forma en como están distribuidas las letras y los caracteres"
+    },
+    {
+      letter: "r",
+      starts_or_contains: "start",
+      header: "empieza por la",
+      def: "dispositivo de hardware que permite la interconexión de ordenadores en red, que determina el siguiente punto hacia donde se dirigen los datos",
+      answer: "router",
+      tip: "aparato que nos facilita nuestra compañía de teléfono que nos provee acceso a la red local y a internet de forma inalámbrica"
+    },
+    {
+      letter: "s",
+      starts_or_contains: "start",
+      header: "empieza por la",
+      def: "programa especial que se carga en un ordenador al encenderlo y cuya función es gestionar los demás programas o aplicaciones que se ejecutarán",
+      answer: "sistema operativo",
+      tip: "windows, linux, unix, MacOS son algunos ejemplos"
+    },
+    {
+      letter: "t",
+      starts_or_contains: "start",
+      header: "empieza por la",
+      def: "formato de archivo que almacena información, especialmente destinado a distribuir archivos de gran tamaño",
+      answer: "torrent",
+      tip: "almacena los datos necesarios para que una aplicación de BitTorrent comparta el contenido"
+    },
+    {
+      letter: "u",
+      starts_or_contains: "start",
+      header: "empieza por la",
+      def: "persona que tiene una cuenta por medio de la cual puede acceder a los recursos y servicios que ofrece una red",
+      answer: "usuario",
+      tip: "a las redes sociales accedemos con el nombre con el que iniciamos el registro y con la contraseña"
+    },
+    {
+      letter: "w",
+      starts_or_contains: "start",
+      header: "empieza por la",
+      def: "websites que permiten a los usuarios modificar o crear contenido de forma rápida y sencilla",
+      answer: "wiki",
+      tip: "estas páginas se desarrollan a partir de la colaboración de los internautas y el término se popularizó con el auge de la wikipedia"
+    },
+    /*{
+      letter: "x",
+      starts_or_contains: "contain",
+      header: "contiene la",
+      def: "servidor especial encargado de centralizar el tráfico entre internet y una red privada, de forma que evita que cada una de las máquinas de la red interior tenga que disponer necesariamente de una conexión directa a la red",
+      answer: "proxy",
+      tip: ""
+    },*/
+    {
+      letter: "y",
+      starts_or_contains: "contain",
+      header: "contiene la",
+      def: "término inglés que se refiere al acto de atormentar y molestar a otra persona usando medios electrónicos",
+      answer: "cyberbullying",
+      tip: "acoso psicológico entre iguales en internet y redes sociales, suele derivar del acoso escolar"
     }
 	]
 };
--- app/locales.js
+++ app/locales.js
@@ -1,10 +1,33 @@
 export const LOCALES = {
  en:{
-   "hola":"hello",
    "adios":"bye",
+   "hola":"hello",
+   "aceptar": "accept",
+   "aciertos": "hits", //right guess
+   "cancelar": "cancel",
+   "créditos": "credits",
+   "empezar": "start",
+   "empezar la prueba": "start the game",
+   "¡enhorabuena! terminaste": "congratulations! you finished",
+   "eres un crack, ¡a ti no hay quien te engañe! sabemos que no necesitas que te enseñemos uso seguro TIC e internet, pero aquí te dejamos un pequeño vídeo por si quieres saber más:": "you are a rock start, no one fools you! we know that you do not need us to teach you safe use of ICT and internet, but here we leave you a small video in case you want to know more:",
+   "¿estás seguro de que quieres parar y finalizar la prueba? todavía tienes palabras sin contestar:": "are you sure you want to stop and finish the game? you still have missing words:",
+   "¿estás seguro de que quieres reiniciar la prueba? esta acción eliminará todo tu progreso.": "are you sure you want to restart the game? this action will eliminate all your progress.",
+   "finaliza la prueba": "end the game",
+   "finalizar": "end up",
+   "has terminado la prueba. puedes repasar las palabras acertadas.": "you have finished the game. you can review the correct words.",
+   "lo has hecho bastante bien, pero has fallado algunas. revisa el siguiente video para aprender un poco más:": "you have done quite well, but you have failed some. check the following video to learn a little more:",
+   "muestra el progreso de tareas": "show the progress of the game",
+   "muestra las intrucciones": "show instructions",
+   "no has acertado muchas. es muy importante que aprendas uso seguro TIC e internet. te dejamos un video para que aprendas un poco más:": "you have not guessed many words. it is very important you learn safe ICT and internet use. we leave you a video here, so you can learn a little more:",
+   "para comprobar una palabra pulsa intro o haz click en el botón de check. tienes un intento, pero puedes ayudarte de los comodines si te atascas. los controles son muy sencillos:": "to check a word press enter or click on the check button. you have an attempt, but you can help yourself from the wildcards if you get stuck. the controls are very simple:",
+   "puntos": "score"
+   "reinicia la prueba": "restart the game",
+   "¿te atreves con este reto? tienes una hora para completar y superar nuestro muro de palabras, sobre uso seguro de internet y las TIC": "do you dare with this challenge? you have one hour to complete and overcome our word wall, about the safe use of the internet and ICT",
+   "tiempo": "time",
+   "una aplicación de": "an application of",
+   "ver las respuestas": "see the answers",
+   "ver más apps": "see more apps",
  },
- ru:{
- 	"hola":"rusola",
-  "adios": "adiosruso"
- }
+
 };
+
