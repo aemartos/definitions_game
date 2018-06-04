61e29e2fcfd77713f2f601f0b83ce403018cbf46
aemartos <aemartos@gmail.com>
2018-06-04 11:30:42 +0200

    Responsive done

--- app/index.js
+++ app/index.js
@@ -37,8 +37,16 @@ function init_app(){
 	
 }
 
+$(document).ready(function(){
+	init_app();
 
-init_app();
+	mediaquery1.addListener(handleOrientationChange);
+	mediaquery2.addListener(handleOrientationChange);
+	handleOrientationChange();
+	removeHover();
+});
+
+
 
 
 
@@ -46,14 +54,8 @@ init_app();
 
 // -------- REMOVE HOVER ONTOUCH -----------
  
-
-  function hasTouch() {
-    return 'ontouchstart' in document.documentElement
-      || navigator.maxTouchPoints > 0
-      || navigator.msMaxTouchPoints > 0;
-  }
-
-  if (hasTouch()) { // remove all :hover stylesheets
+function removeHover(){
+  if ('ontouchstart' in document.documentElement || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0) { 
     try { // prevent exception on browsers not supporting DOM styleSheets properly
       for (var si in document.styleSheets) {
         var styleSheet = document.styleSheets[si];
@@ -69,7 +71,7 @@ init_app();
       }
     } catch (ex) {}
   }
-
+}
 
 // ------------ OWL CROUSEL MEDIA ----------------
 
@@ -77,6 +79,7 @@ var mediaquery1 = matchMedia("(max-width: 1024px) and (orientation: portrait)");
 var mediaquery2 = matchMedia("(max-width: 980px)");
 
 function handleOrientationChange(mediaquery) {
+	console.log("holi");
   if (mediaquery1.matches || mediaquery2.matches) {
 
 		$('.owl-carousel').owlCarousel({
@@ -87,32 +90,14 @@ function handleOrientationChange(mediaquery) {
     //nav: true,
 		navText: ["<div class='owl-prev'><img class='control control_left_arrow svg'src='/assets/images/icons/left_arrow_fill.svg'/></div>","<div class='owl-next'><img class='control control_right_arrow svg'src='/assets/images/icons/right_arrow_fill.svg'/></div>"],
     responsiveClass: true,
-    /*responsive: {
-    	'(max-width: 412px) and (orientation: portrait)': {
-	      items: 1,
-	      nav:true,
-        dots: false
-    	},
-    	'(max-width: 768px) and (orientation: portrait)': {
-	      items: 4,
-	      nav:false
-    	},
-    	'(max-width: 1024px) and (orientation: portrait)': {
-	      items: 6,
-	      loop:false
-    	}
-  	}*/
+    responsiveBaseElement: ".word-wall",
     responsive:{
         0:{
             items:1,
             nav:true,
             dots: false
         },
-        /*500:{
-            items:4,
-            nav:false
-        },*/
-        750:{
+        500:{
             items:4,
             nav:false
         },
@@ -128,8 +113,6 @@ function handleOrientationChange(mediaquery) {
   }
 }
 
-mediaquery1.addListener(handleOrientationChange);
-mediaquery2.addListener(handleOrientationChange);
 
 
 
